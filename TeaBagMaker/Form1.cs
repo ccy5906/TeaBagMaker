b81c20cf3e0de051db9c66106b065203d3cde696
Jiyeon Choi <ccy5906@naver.com>
2019-05-27 09:33:42 +0900

    #1 콤보박스, 버튼, 텍스트 박스 추가

--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -11,7 +11,7 @@ namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
-        string[] TeaList = new string[] { "홍차(2분)", "녹차(3분)", "루이보스차(5분)", "국화차(2분)" };
+       
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +19,7 @@ namespace TeaBagMaker
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            for(int i=0; i < TeaList.Length; i++)
-            {
-                this.TeaCombo.Items.Add(TeaList[i]);
-            }
-            this.TeaCombo.SelectedIndex
+            
         }
     }
 }
