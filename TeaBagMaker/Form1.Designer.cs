77af6e471bf8739a3d19529d9fb4d3e170d8f057
Jiyeon Choi <ccy5906@naver.com>
2019-05-27 10:06:09 +0900

    #2 차 선택에 따른 타이머 설정, 확인버튼 클릭시 리셋, 타이머 되는 곳 양식 지정

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -28,63 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.TeaCombo = new System.Windows.Forms.ComboBox();
             this.but_add = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Countdown = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.butOK = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // TeaCombo
             // 
             this.TeaCombo.Font = new System.Drawing.Font("나눔고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.TeaCombo.FormattingEnabled = true;
-            this.TeaCombo.Location = new System.Drawing.Point(34, 12);
+            this.TeaCombo.Location = new System.Drawing.Point(12, 12);
             this.TeaCombo.Name = "TeaCombo";
-            this.TeaCombo.Size = new System.Drawing.Size(140, 25);
+            this.TeaCombo.Size = new System.Drawing.Size(178, 25);
             this.TeaCombo.TabIndex = 0;
             // 
             // but_add
             // 
             this.but_add.Font = new System.Drawing.Font("나눔고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.but_add.Location = new System.Drawing.Point(34, 51);
+            this.but_add.Location = new System.Drawing.Point(12, 54);
             this.but_add.Name = "but_add";
-            this.but_add.Size = new System.Drawing.Size(141, 41);
+            this.but_add.Size = new System.Drawing.Size(178, 41);
             this.but_add.TabIndex = 1;
             this.but_add.Text = "담그기!";
             this.but_add.UseVisualStyleBackColor = true;
+            this.but_add.Click += new System.EventHandler(this.But_add_Click);
             // 
-            // textBox1
+            // Countdown
             // 
-            this.textBox1.Font = new System.Drawing.Font("나눔고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.textBox1.Location = new System.Drawing.Point(35, 104);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(140, 32);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Countdown.Font = new System.Drawing.Font("나눔고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Countdown.Location = new System.Drawing.Point(12, 113);
+            this.Countdown.Name = "Countdown";
+            this.Countdown.ReadOnly = true;
+            this.Countdown.Size = new System.Drawing.Size(178, 25);
+            this.Countdown.TabIndex = 2;
+            this.Countdown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("나눔고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(30, 166);
+            this.label1.Location = new System.Drawing.Point(25, 215);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(155, 15);
             this.label1.TabIndex = 3;
             this.label1.Text = "2019.05.27 3517 최지연";
             // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // butOK
+            // 
+            this.butOK.Font = new System.Drawing.Font("나눔고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.butOK.Location = new System.Drawing.Point(13, 158);
+            this.butOK.Name = "butOK";
+            this.butOK.Size = new System.Drawing.Size(177, 36);
+            this.butOK.TabIndex = 4;
+            this.butOK.Text = "확인";
+            this.butOK.UseVisualStyleBackColor = true;
+            this.butOK.Click += new System.EventHandler(this.ButOK_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(202, 190);
+            this.ClientSize = new System.Drawing.Size(202, 253);
+            this.Controls.Add(this.butOK);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Countdown);
             this.Controls.Add(this.but_add);
             this.Controls.Add(this.TeaCombo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "차 선택하기";
+            this.Text = "차선택";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -95,8 +117,10 @@
 
         private System.Windows.Forms.ComboBox TeaCombo;
         private System.Windows.Forms.Button but_add;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Countdown;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button butOK;
     }
 }
 
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,7 +12,11 @@ namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
-       
+        string[] TeaList = new string[] { "홍차(2분)", "녹차(3분)", "루이보스차(5분)", "국화차(2분)" };
+        int teaName = 0;
+        int CountOrgNum = 0;
+        int min, sec;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +24,72 @@ namespace TeaBagMaker
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            for (int i = 0; i < TeaList.Length; i++)
+            {
+                this.TeaCombo.Items.Add(TeaList[i]);
+            }
+            this.TeaCombo.SelectedIndex = 0;
+        }
+
+        private void But_add_Click(object sender, EventArgs e)
+        {
+            if (this.TeaCombo.SelectedIndex == 0)
+            {
+                teaName = 0;
+                CountOrgNum = 120;
+            }
+            else if(this.TeaCombo.SelectedIndex == 1)
+            {
+                teaName = 1;
+                CountOrgNum = 180;
+            }
+            else if (this.TeaCombo.SelectedIndex == 2)
+            {
+                teaName = 2;
+                CountOrgNum = 300;
+            }
+            else if (this.TeaCombo.SelectedIndex == 3)
+            {
+                teaName = 3;
+                CountOrgNum = 120;
+            }
+            this.timer.Enabled = true;
+        }
+
+        private void ButOK_Click(object sender, EventArgs e)
+        {
+            this.timer.Enabled = false;
+            this.Countdown.Text = "";
+            this.TeaCombo.Focus();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if(this.CountOrgNum < 1)
+            {
+                this.timer.Enabled = false;
+                MessageBox.Show("티백을 건지세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);  
+                this.TeaCombo.Focus();
+                this.Countdown.Text = "";
+            }
+            else
+            {
+                this.CountOrgNum--;
+                
+                min = CountOrgNum / 60;
+                sec = CountOrgNum % 60;
+                if (min == 0)
+                {
+                    this.Countdown.Text = Convert.ToString( sec + "초 남았습니다!");
+                }
+                else
+                {
+                    this.Countdown.Text = Convert.ToString(min + "분 " + sec + "초 남았습니다!");
+                }
+                
+               //this.Countdown.Text = Convert.ToString(CountOrgNum);
+
+            }
         }
     }
 }
